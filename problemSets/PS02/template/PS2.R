8716beb35525f5bdfc1268c57a7b5ec0be4c9f63
oflaheci <47536782+oflaheci@users.noreply.github.com>
2023-02-19 20:02:51 +0000

    PS02

--- problemSets/PS02/template/PS2.R
+++ problemSets/PS02/template/PS2.R
@@ -1,6 +1,7 @@
 #####################
 # load libraries
 # set wd
+# setwd(getwd())
 # clear global .envir
 #####################
 
@@ -30,7 +31,8 @@ pkgTest <- function(pkg){
 lapply(c(),  pkgTest)
 
 # set wd for current folder
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+# setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+setwd("/Users/ciaraoflaherty/StatsII_Spring2023/problemSets/PS02/template")
 
 #####################
 # Problem 1
@@ -38,3 +40,63 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 # load data
 load(url("https://github.com/ASDS-TCD/StatsII_Spring2023/blob/main/datasets/climateSupport.RData?raw=true"))
+dat <- climateSupport
+head(dat)
+summary(dat)
+unique(dat$choice)
+
+# clean data 
+# change to boolean 
+dat$choice <- sub("Not supported",0, dat$choice)
+dat$choice <- sub("Supported", 1, dat$choice)
+unique(dat$choice)
+dat$choice <- as.logical(as.numeric(dat$choice))
+typeof(dat$choice)
+
+unique(dat$sanctions)
+dat$sanctions <- sub("None", 0, dat$sanctions)
+dat$sanctions <- sub("%","", dat$sanctions)
+dat$sanctions <- as.factor(dat$sanctions)
+
+unique(dat$countries)
+dat$countries <- sub(" of 192", "", dat$countries)
+dat$countries <- as.factor(dat$countries)
+
+# part 1 - additive model 
+modl <- glm(choice ~ countries + sanctions, data = dat,
+            family = "binomial")
+predict(modl, dat)
+stargazer::stargazer(modl, type = "text")
+summary(modl)
+
+nmodl <- glm(choice ~ 1, data = dat, family = binomial(link = "logit"))
+test2 <- anova(nmodl, modl, test = "LRT")
+summary(test2)
+test2 # reject the null 
+
+# modl:
+# Y_hat = Intercept + Beta1aX1 + Beta1bX1 +Beta1cX1 + Beta2aX2 + Beta2bX2 + Beta2cX2
+# yhat = Intercept + (countries20 + countries80 + countries160)X1 _ (sanctions15 + sanctions20 + sanctions5)X2
+# Y_hat = 0.376 + -0.648(0 or 1) - 0.312(0 or 1) - 0.133(0 or 1) - 0.304(0 or 1) + 0.192(0 or 1)
+# Y_hat = 0.376 - 0.133(0 or 1) + 0.192(0 or 1)
+# Y_hat_5% = 0.376 + 0.192(1) = 0.568
+# Y_hat_5% = 0.376 - 0.133(1) = 0.243
+# 0.325 difference in log odds
+# prob = odds / (1 + odds)
+#     = -0.325 / (1 - 0.325) = -0.4814815 probability 
+
+# Y_hat = 0.376 - 0.312(1) = 0.064
+# 0.064 / (1 + 0.064) = 0.06015038 probability 
+
+# part 2c 
+modl1 <- glm(choice ~ countries * sanctions, dat, family = "binomial")
+compar <- anova(modl, modl1, test = "LRT")
+# summary(compar)
+compar # fail to reject H0 
+
+
+
+
+
+
+
